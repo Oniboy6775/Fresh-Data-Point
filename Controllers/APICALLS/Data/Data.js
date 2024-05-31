47ed7b8c0be8bff8314d911e73af189a64a38be4
oniboy6775 <onisabiabdullahi@gmail.com>
2024-05-31 08:53:19 +0100

    worked locally

--- Controllers/APICALLS/Data/Data.js
+++ Controllers/APICALLS/Data/Data.js
@@ -2,9 +2,11 @@ const axios = require("axios");
 
 const BUYDATA = async ({ network, mobile_number, plan }) => {
   console.log({ "req-key": process.env.DATARELOADED_API_KEY });
+  console.log({ "req-URL": process.env.DATARELOADED_API });
   try {
     const BuyDataResponse = await axios.post(
       `${process.env.DATARELOADED_API}/buy/data`,
+      // `http://localhost:4000/api/v1/buy/data`,
       {
         network: network,
         mobile_number: mobile_number,
@@ -22,10 +24,10 @@ const BUYDATA = async ({ network, mobile_number, plan }) => {
       data: BuyDataResponse.data.receipt,
     };
   } catch (error) {
-    console.log(error.response.data);
+    // console.log(error.response);
     return {
       status: false,
-      msg: error.response.data.msg || "Transaction failed",
+      msg: error.response?.data.msg || "Transaction failed",
     };
   }
 };
